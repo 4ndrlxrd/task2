4b109f8a7a08d110083aae2dc5f9dc59c8cee1c0
4ndrlxrd <youvags@gmail.com>
2023-02-09 23:23:00 +0300

    first commit

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	int a, b;
+	printf("a=");
+	std::cin >>a ;
+	printf("b=");
+	std::cin >> b;
+	if (a > b)
+	{
+		printf("A bolshe");
+	}
+	else if (b > a)
+	{
+		printf("B bolshe");
+	}
+	else
+	{
+		printf("A = B");
+	}
+	return 0;
+}
